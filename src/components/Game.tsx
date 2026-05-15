543bcddf8112d557f14bf5cf340bd9f30e800977
Aufa Wicaksono <aufawicaksono18@gmail.com>
2026-05-16 02:18:01 +0700

    Refactor app structure and implement game logic
    - Replace App component with Game component wrapped in GestureHandlerRootView.
    - Remove index.ts and update package.json to point to Expo's AppEntry.js.
    - Add Game and Snake components for game functionality.
    - Introduce utility function checkGameOver and types for coordinates and directions.
    - Add color styles for the game.

---
+++ src/components/Game.tsx
@@ -0,0 +1,108 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { Colors } from "../styles/colors";
+import { Coordinate, Direction } from "../types/types";
+import Snake from "./Snake";
+import { checkGameOver } from "../utils/checkGameOver";
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 1, xMax: 38, yMin: 1, yMax: 83 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
+
+export default function Game() {
+  const [direction, setDirection] = React.useState<Direction>(Direction.Right);
+  const [snake, setSnake] = React.useState<Coordinate[]>(
+    SNAKE_INITIAL_POSITION,
+  );
+  const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION);
+  const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+  const [isPaused, setIsPaused] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    if (!isGameOver) {
+        const intervalId = setInterval(() => {
+            !isPaused && moveSnake();
+        }, MOVE_INTERVAL);
+        return () => clearInterval(intervalId);
+    }
+  }, [snake, isGameOver, isPaused]);
+
+  const moveSnake = () => {
+    const snakeHead = snake[0];
+    const newHead = { ...snakeHead }; // creating a copy
+
+    if (checkGameOver(snakeHead, GAME_BOUNDS)) {
+        setIsGameOver((prev) => !prev);
+        return;
+    }
+
+    switch (direction) {
+      case Direction.Up:
+        newHead.y -= 1;
+        break;
+      case Direction.Down:
+        newHead.y += 1;
+        break;
+      case Direction.Left:
+        newHead.x -= 1;
+        break;
+      case Direction.Right:
+        newHead.x += 1;
+        break;
+      default:
+        break;
+    }
+
+    // if eats food
+    // grow snake
+
+    setSnake([newHead, ...snake.slice(0, -1)]);
+  };
+
+  const panGesture = Gesture.Pan().onEnd((event) => {
+    const { translationX, translationY } = event;
+
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        setDirection(Direction.Right);
+      } else {
+        setDirection(Direction.Left);
+      }
+    } else {
+      if (translationY > 0) {
+        setDirection(Direction.Down);
+      } else {
+        setDirection(Direction.Up);
+      }
+    }
+  });
+
+  return (
+    <GestureDetector gesture={panGesture}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.boundaries}>
+          <Snake snake={snake} />
+        </View>
+      </SafeAreaView>
+    </GestureDetector>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  },
+  boundaries: {
+    flex: 1,
+    borderColor: Colors.primary,
+    borderWidth: 12,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    backgroundColor: Colors.background,
+  },
+});
